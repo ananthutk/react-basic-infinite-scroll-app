61e8b202e03b8768fca2d1f4816d019f3bb12c50
ananthutk <ananthutk97@gmail.com>
2021-11-27 19:48:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState, useRef, useCallback } from 'react';
 import './App.css';
+import useBookSearch from './useBookSearch';
 
-function App() {
+const App = () => {
+
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return;
+
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        console.log('visible');
+        setPageNumber(prevPageNumber => prevPageNumber + 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+
+    console.log(node);
+
+  }, [loading, hasMore])
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={query}
+        onChange={handleSearch}
+      />
+
+      <ul>
+        {books.map((book, index) => {
+          if (books.length === index + 1) {
+            return <li ref={lastBookElementRef} key={book}>{book}</li>
+          }
+          else {
+            return <li key={book}>{book}</li>
+          }
+        })}
+      </ul>
+
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error...'}</div>
     </div>
   );
 }
